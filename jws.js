19ff03f937c097f2bd0907b876ff7bf011361bcf
jwsnow <44217047+jwsnow@users.noreply.github.com>
2019-01-02 19:49:25 -0600

    Add files via upload

---
+++ jws.js
@@ -0,0 +1,13 @@
+
+function writeHeader(){
+	document.write('\
+<div id="masterDiv">\
+<div id="topNavDiv"> <div id="topNavDivContent">\
+</div></div>\
+<div id="mainContentDiv">	\
+	');
+}
+
+function writeFooter(){
+	document.write('</div><div id="bottomNavDiv"><a class="plain" href="index.html">home</a></div></div>');
+}
